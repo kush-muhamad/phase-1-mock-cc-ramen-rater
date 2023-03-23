9756a72c30dc2d07273b31c8311742a8396ff0f9
kush Muhamad <kushmohammed98@yahoo.com>
2023-03-24 00:24:18 +0300

    Task1 done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// write your code here
+// where our dom content loaded resides
+document.addEventListener("DOMContentLoaded",function() {
+    fetch("http://localhost:3000/ramens")//fetch the data 
+    .then(res => res.json()) // changing the response to json
+    .then(ramens =>{
+        const ramenMenu = document.querySelector("#ramen-menu")
+        for (const element of ramens) {
+            let image = document.createElement("img")
+            image.src = element.image
+
+            ramenMenu.appendChild(image)
+            
+        }
+    })
+    
+})
